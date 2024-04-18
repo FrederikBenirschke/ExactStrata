c3c2c816b71bd2b4a940e78f3aa19c2fb072205d
Frederik Benirschke <133478072+FrederikBenirschke@users.noreply.github.com>
2024-04-18 15:05:18 -0500

    Update divtautpolynomial.py

--- exactstrata/divtautpolynomial.py
+++ exactstrata/divtautpolynomial.py
@@ -19,10 +19,11 @@ from sage.calculus.var import var
 
 
 
-# Tuple of symbolic expressions in the variables xi and D_i where i is the index of a BIC
-# Representing a polynomial in divisorial classes
-# We want DivTautPolynomial to be cachable, so need to work with tuples
+
 class DivTautPolynomial(SageObject):
+    ''' A DivTautPolynomial is a tuple where each entry is a SymbolicExpression, i.e. a polynomial  in the variables xi and D_i, where i is the index of a BIC.
+    A DivTautPolynomial formally represents a tautological class in the Chow ring of the moduli space of multi-scale differentials.
+    DivTautPolynomial needs to be cachable and its thue implemented using tuples.'''
     
     def __init__(self, div_taut_poly):
         self._div_taut_poly = div_taut_poly
@@ -68,11 +69,13 @@ class DivTautPolynomial(SageObject):
     
 
     def __add__(self, other):
+        ''' Returns the sum of two DivTautPolynomials (as DivTautPolynomial).'''
         S = SR['t']; (t,) = S._first_ngens(1)
         return DivTautPolynomial((S(self.div_taut_poly) + S(other.div_taut_poly)).list())
         
     
     def __mul__(self, other):
+        ''' Returns the product of two DivTautPolynomials (or of a DivTautPolynomial with an integer). The result is a DivTautPolynomial.'''
         if isinstance(other, sage.rings.integer.Integer):
             return DivTautPolynomial([coeff*other for coeff in self.div_taut_poly ])
         if not isinstance(other, DivTautPolynomial):
@@ -86,26 +89,32 @@ class DivTautPolynomial(SageObject):
             return DivTautPolynomial([coeff*other for coeff in self.div_taut_poly ])
         
     def __sub__(self, other):
+        ''' Returns the difference of two DivTautPolynomials. '''
         S = SR['t']; (t,) = S._first_ngens(1)
         return DivTautPolynomial((S(self.div_taut_poly) - S(other.div_taut_poly)).list())
         
         
     
     def __pow__(self, exponent):
+        ''' Raises a DivTautPolynomial to the power exponent.'''
         S = SR['t']; (t,) = S._first_ngens(1)
         return DivTautPolynomial(((S(self.div_taut_poly))**exponent).list())
         
         
     @property
     def codim(self):
+        ''' The degree of the DivTautPolynomial. In the case that DivTautPolynomials represent the normal bundle of a subvariety
+        this agree with the codimension. '''
         return len(self.list())
     
     
     def reciprocal(self):
+        ''' Reverses the list of coefficients of the polynomial.'''
         return DivTautPolynomial(tuple(reversed(self.div_taut_poly)))
     
     
     def substitute(self, old_variables, new_variables, verbose = False):
+        ''' Replaces the variables of DivTautPolynomial by a different set of variables.'''
         if len(old_variables) != len(new_variables):
                 show(old_variables, new_variables)
                 raise ValueError("Number of variables is not the same")
@@ -121,6 +130,7 @@ class DivTautPolynomial(SageObject):
     
     
     def proper_transform(self, var):
+        ''' Implements Fulton's formula for the class of a proper transform under a blow-up.'''
         S = SR['t']; (t,) = S._first_ngens(1)
         poly =  S(self.div_taut_poly)
         return DivTautPolynomial(poly(t= S(t+var)).list())
@@ -131,9 +141,10 @@ class DivTautPolynomial(SageObject):
    
     
     
-    # Inverts the DivTautPolynomial in the Chow ring CH^*(X)
-    # dim - dimension of ambient space = maximal length of the inverse - 1
+    
     def invert(self, dim):
+        ''' Inverts the DivTautPolynomial in the Chow ring CH^*(X).
+        dim - dimension of ambient space = maximal length of the inverse - 1.'''
         # Leading term must be non-zero
         assert self[ 0 ]!= 0 
         Z = PowerSeriesRing(SR, 'z', default_prec=dim+ 1 , names=('z',)); (z,) = Z._first_ngens(1)
@@ -142,13 +153,16 @@ class DivTautPolynomial(SageObject):
         return DivTautPolynomial(inverted)
     
     def divide(self, other, length):
+        ''' Retursn the division of DivTautPolynomial by another one.
+        ''' Requires the expected length of the result as additional parameter.'''
         Z = PowerSeriesRing(SR, 'z', default_prec=length+ 1 , names=('z',)); (z,) = Z._first_ngens(1)
         inverted = ( 1 / Z(self.div_taut_poly)).list()
         return (self * other.invert(length)).trim(length)
         
     
-    # Ensure that the polynomial has degree (i.e. codim) at most k
+    
     def trim(self, length):
+        ''' Ensures that the polynomial has degree (i.e. codim) at most k.'''
         # Codimension is greather than length?
         if length >= 0  and length <= len(self.list()) - 1 :
             #Then cut off the remainder
@@ -159,9 +173,10 @@ class DivTautPolynomial(SageObject):
         
     
     
-    # Makes sure the tuple has the right length
-    # Since DivTautPolynomial should be hashable we are returning a new DivTautPolynomial
+    
     def extend_to_length(self, target_len):
+        ''' Returns a new DivTautPolynomial by adding zeros until the tuple encoding the DivTautPolynomial has the desired length.
+        Since DivTautPolynomial needs to be hashable, the result is a new DivTautPolynomial. '''
         new_poly =  self.tuple()[:target_len] + ( 0 ,)*(target_len - len(self.div_taut_poly))
         return DivTautPolynomial(new_poly)
      
